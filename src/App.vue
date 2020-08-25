f6491e491b8aff31dfe9da992a32934cf0df4beb
marekboro <marek.borowicz83@gmail.com>
2020-08-25 23:08:59 +0100

    ok so filtering works, styling works, adding tweets works, mvp done, and so am I, I am not solving the ID crisis everyone mentioned on slack

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 
 <template>
   <div>
-    <section>
-    <form v-on:submit.prevent="addTweet">
-    <h4>Tweet a tweet</h4>
-    <input placeholder="Your name" type="text" v-model="newTweet.name">
-    <input placeholder="Your handle" type="text" v-model="newTweet.handle">
-    <input placeholder="Your tweet" type="text" v-model="newTweet.tweet">
-    <input type="submit" value="Tweet">
-    </form>
+    <section id="totalTweets">
+      <p>Total Likes:{{totalLikes}}</p>
     </section>
-    <h1>App Page</h1>
-    <tweet-list-item v-for="(tweet,index) in tweets" :key="index" :tweetx="tweet"></tweet-list-item>
-    <p>{{totalLikes}}</p>
+
+    <section id="tweeting">
+      <form v-on:submit.prevent="addTweet">
+        <h4>Tweet a tweet</h4>
+        <input placeholder="Your name" type="text" v-model="newTweet.name" />
+        <input placeholder="Your handle" type="text" v-model="newTweet.handle" />
+        <input placeholder="Your tweet" type="text" v-model="newTweet.tweet" />
+        <input type="submit" value="Tweet" />
+      </form>
+    </section>
+    <section id="Filter">
+      <form v-on:submit.prevent="filterByLikes">
+        <h4>Fileter by likes:</h4>
+        <input type="number" v-model.number="likesFilterAmount" />
+        <input type="submit" value="Filter" />
+      </form>
+    </section>
+  
+    <section>
+      <tweet-list-item v-for="(tweet,index) in filterByLikes" :key="index" :tweetx="tweet"></tweet-list-item>
+    </section>
   </div>
 </template>
 
 <script>
-import TweetListItem from './components/TweetListItem.vue' 
+import TweetListItem from "./components/TweetListItem.vue";
 export default {
-  name: 'app',
+  name: "App",
   data() {
     return {
       tweets: [
@@ -48,40 +60,49 @@ export default {
           likes: 18,
         },
       ],
-      newTweet:{
-        id:0,
-        name:"",
-        handle:"",
+      newTweet: {
+        id: 0,
+        name: "",
+        handle: "",
         img: "https://semantic-ui.com/images/avatar2/large/elyse.png",
-        tweet:"",
-        likes:0
+        tweet: "",
+        likes: 0,
       },
+      likesFilterAmount: 0,
     };
   },
-  computed:{
-    totalLikes: function(){
-      return this.tweets.reduce((total,tweet) => {
-        return total+tweet.likes
-      },0)
-    }
+  computed: {
+    totalLikes: function () {
+      return this.tweets.reduce((total, tweet) => {
+        return total + tweet.likes;
+      }, 0);
+    },
+    // filterByLikes: function(){
+    //   return this.tweets.filter(tweet => tweet.likes >= this.likesFilterAmount)
+    // }
+    filterByLikes: function () {
+      return this.tweets.filter((tweet) => {
+       return tweet.likes >= this.likesFilterAmount;
+      });
+      this.likesFilterAmount = 0;
+    },
   },
-  components:{
-    'tweet-list-item':TweetListItem
+  components: {
+    "tweet-list-item": TweetListItem,
   },
-  methods:{
-    addTweet: function(){
-      
+  methods: {
+    addTweet: function () {
       this.tweets.unshift(this.newTweet);
-      this.newTweet={
-        id:"",
-        name:"",
-        handle:"",
+      this.newTweet = {
+        id: "",
+        name: "",
+        handle: "",
         img: "https://semantic-ui.com/images/avatar2/large/elyse.png",
-        tweet:"",
-        likes:0
+        tweet: "",
+        likes: 0,
       };
-    }
-  }
+    },
+  },
 };
 </script>
 
