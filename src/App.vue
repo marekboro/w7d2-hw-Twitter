c268efee6cf44965bdd1dd76b66d7fa0e3514a47
marekboro <marek.borowicz83@gmail.com>
2020-08-25 17:47:04 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>App Page</h1>
+    <p>{{totalLikes}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+      tweets: [
+        {
+          id: 1,
+          name: "James",
+          handle: "@jokerjames",
+          img: "https://semantic-ui.com/images/avatar2/large/matthew.png",
+          tweet: "If you don't succeed, dust yourself off and try again.",
+          likes: 10,
+        },
+        {
+          id: 2,
+          name: "Fatima",
+          handle: "@fantasticfatima",
+          img: "https://semantic-ui.com/images/avatar2/large/molly.png",
+          tweet: "Better late than never but never late is better.",
+          likes: 12,
+        },
+        {
+          id: 3,
+          name: "Xin",
+          handle: "@xeroxin",
+          img: "https://semantic-ui.com/images/avatar2/large/elyse.png",
+          tweet: "Beauty in the struggle, ugliness in the success.",
+          likes: 18,
+        },
+      ],
+    };
+  },
+  computed:{
+    totalLikes: function(){
+      return this.tweets.reduce((total,tweet) => {
+        return total+tweet.likes
+      },0)
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
